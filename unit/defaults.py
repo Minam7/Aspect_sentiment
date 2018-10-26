085c0bcd496205f9485c4172a2a019426631cf94
mina <mina.m777@yahoo.com>
2018-10-26 21:56:20 +0330

    add aspect analysis previous work

--- unit/defaults.py
+++ unit/defaults.py
@@ -1,4 +1,4 @@
-FULL_DATA_PATH = '../data/results.jl'
+FULL_DATA_PATH = 'data/results.jl'
 MODEL_PATH = 'models/model.h5'
 PROCESSED_PICKLE_DATA_PATH = 'processed_data.pickle'
 COMMENT_MAX_LENGTH = 128
